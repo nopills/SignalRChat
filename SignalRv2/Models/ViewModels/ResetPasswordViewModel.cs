8eede3b4399a5154dd4b662b0362654a6e83393b
noPills <67781595+nopills@users.noreply.github.com>
2020-11-23 14:28:37 +0300

    Add ForgotPassword and ResetPassword methods

---
+++ SignalRv2/Models/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,27 @@
+﻿using SignalRv2.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRv2.Models.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        [Required(ErrorMessage = "Please enter a password")]
+        [StringLength(Constants.MaxPassLength, ErrorMessage = "{0} must be between {2} and {1} character", MinimumLength = Constants.MinPassLength)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please repeat your password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
