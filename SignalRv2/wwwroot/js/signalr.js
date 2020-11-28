13d2d4aaf4a40a800ce8e5f3b48e69d9b312bf05
noPills <67781595+nopills@users.noreply.github.com>
2020-11-28 19:06:21 +0300

    Add message formatting

--- SignalRv2/wwwroot/js/signalr.js
+++ SignalRv2/wwwroot/js/signalr.js
@@ -1,11 +1,15 @@
-﻿var chat_body = document.querySelector('.chat-body');
+﻿let chat_body = document.querySelector('.chat-body');
 let sended = true;
 
 
 function RecieveMessage(name, message) {
-    var time = new Date();
-    time = time.toLocaleTimeString([], { timeStyle: 'short' });
+    let time = new Date();
     let msgData = document.createElement("div");
+    time = time.toLocaleTimeString([], { timeStyle: 'short' });
+    
+    message = message.replace(/<\/?[^>]+(>|$)/g, "");
+    time = time.replace(/<\/?[^>]+(>|$)/g, "");
+    name = name.replace(/<\/?[^>]+(>|$)/g, "");
     msgData.className = "message-item";
     msgData.innerHTML = `<figure class="avatar">
                              <img src="/img/avatars/man_avatar4.jpg" class="avatar avatar-circle" alt="dialog-avatar">
@@ -26,26 +30,31 @@ function RecieveMessage(name, message) {
 }
 
 function AddMessage(message) {
-    var time = new Date();
-    time = time.toLocaleTimeString([], { timeStyle: 'short' });
-    let msgData = document.createElement("div");
-    msgData.className = "message-item outgoing-message";
-    msgData.innerHTML = `<div class="message">
+    if (message.trim() !== "" ) {
+        let time = new Date();
+        let msgData = document.createElement("div");
+        time = time.toLocaleTimeString([], { timeStyle: 'short' });
+        time = time.replace(/<\/?[^>]+(>|$)/g, "");
+        message = message.replace(/<\/?[^>]+(>|$)/g, "");
+
+        msgData.className = "message-item outgoing-message";
+        msgData.innerHTML = `<div class="message">
                             <div class="dialog-user-title">
                                 <div class="time">` + time + `<i class="double-checke-message"></i></div>
                                 <h5 class="sender-name">You</h5>
                             </div>
-                            <div class="message-content">`+ message +`</div>
+                            <div class="message-content">`+ message + `</div>
                         </div>
                         <figure class="avatar">
                             <img src="/img/avatars/man_avatar1.jpg" class="avatar avatar-circle" alt="dialog-avatar">
                         </figure>`;
-    
 
-    let msgs = document.querySelector(".messages");
-    msgs.append(msgData);
 
-    chat_body.scrollTop = chat_body.scrollHeight;
+        let msgs = document.querySelector(".messages");
+        msgs.append(msgData);
+
+        chat_body.scrollTop = chat_body.scrollHeight;
+    }
 }
 
 
