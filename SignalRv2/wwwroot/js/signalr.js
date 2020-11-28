d9e0efe940ca42438ab8a55cfd4ce0d9e8473b0b
noPills <67781595+nopills@users.noreply.github.com>
2020-11-28 18:50:58 +0300

    Add result of SendPrivateMessage method

--- SignalRv2/wwwroot/js/signalr.js
+++ SignalRv2/wwwroot/js/signalr.js
@@ -1,13 +1,61 @@
-﻿const connection = new signalR.HubConnectionBuilder()
+﻿var chat_body = document.querySelector('.chat-body');
+let sended = true;
+
+
+function RecieveMessage(name, message) {
+    var time = new Date();
+    time = time.toLocaleTimeString([], { timeStyle: 'short' });
+    let msgData = document.createElement("div");
+    msgData.className = "message-item";
+    msgData.innerHTML = `<figure class="avatar">
+                             <img src="/img/avatars/man_avatar4.jpg" class="avatar avatar-circle" alt="dialog-avatar">
+                             </figure>
+                          <div class="message">
+                            <div class="dialog-user-title">
+                             <h5 class="reciever-name">`+ name +`</h5>
+                             <div class="time">` + time + `<i class="double-checke-message"></i></div>                        
+                            </div>
+                            <div class="message-content">
+                                `+ message + ` 
+                          </div>
+                        </div>`;
+    let msgs = document.querySelector(".messages");
+    msgs.append(msgData);
+
+    chat_body.scrollTop = chat_body.scrollHeight;
+}
+
+function AddMessage(message) {
+    var time = new Date();
+    time = time.toLocaleTimeString([], { timeStyle: 'short' });
+    let msgData = document.createElement("div");
+    msgData.className = "message-item outgoing-message";
+    msgData.innerHTML = `<div class="message">
+                            <div class="dialog-user-title">
+                                <div class="time">` + time + `<i class="double-checke-message"></i></div>
+                                <h5 class="sender-name">You</h5>
+                            </div>
+                            <div class="message-content">`+ message +`</div>
+                        </div>
+                        <figure class="avatar">
+                            <img src="/img/avatars/man_avatar1.jpg" class="avatar avatar-circle" alt="dialog-avatar">
+                        </figure>`;
+    
+
+    let msgs = document.querySelector(".messages");
+    msgs.append(msgData);
+
+    chat_body.scrollTop = chat_body.scrollHeight;
+}
+
+
+// Open Connection
+const connection = new signalR.HubConnectionBuilder()
     .withUrl("/chat")
     .configureLogging(signalR.LogLevel.Information)
     .build();
 
-//Signal method invoked from server
-connection.on("RecieveMessage", (name, message) => {
-    console.clear();
-    console.log(name +": " + message);
-});
+
 
 connection.on("Typing", (message) => { console.log(message); });
 
@@ -15,40 +63,30 @@ connection.on("CheckStatus", (username, status) => {
     console.log(username + " is " + status);
 });
 
-//s = ' ';
-//s = s.replace(/^\s+|\s+$/g, '');
-let sended = true;
 
-let msg = document.getElementById("message");
-    msg.oninput = function () {
-        if (sended == true) {
-            connection.invoke("Typing", "kekushpekush");
-            sended = false;
-        }
-        if (msg.value == '') {
-            console.clear();
-            sended = true;
-        }
+//Subscribe to hub messages
+connection.on("RecieveMessage", (name, message) => {
+         RecieveMessage(name, message);    
+});
 
-    }   
+document.getElementById("sendBtn").addEventListener("click", function (e) {   
+    let message = document.getElementById("messageInput").value;
+    let mockName = document.querySelector('.search-input').value;
+    connection.invoke("SendPrivateMessage", mockName, message).then(res => res == true ? AddMessage(message) : "");
 
+    
+    //if (result != false) {
+    //    AddMessage(message);
+    //    sended = true;
+    //}
+   
+ 
 
-//msg.oninput = function () {
-  //  connection.invoke("Typing");
-//}
-
-
-document.getElementById("sendBtn").addEventListener("click", function (e) {
-   // connection.invoke("SendStatus", 1); //status
+     // connection.invoke("SendStatus", 1); //status
 
     //let res = connection.invoke("GetLastMessages", "e2c0bfd5-0bb8-488c-b5f7-9e414224f435");
     //console.log(res);
-    let username = document.getElementById("username").value;  
-    let message = document.getElementById("message").value;   
-    connection.invoke("SendPrivateMessage", username, message);
-    sended = true;
 });
-
 
 
 
@@ -57,3 +95,32 @@ document.getElementById("sendBtn").addEventListener("click", function (e) {
 connection.start().catch(function (err) {
     return console.error(err.toString());
 });
+
+
+
+
+
+
+
+
+
+
+
+    //msg.oninput = function () {
+    //    if (sended == true) {
+    //        connection.invoke("Typing", "kekushpekush");
+    //        sended = false;
+    //    }
+    //    if (msg.value == '') {
+    //        console.clear();
+    //        sended = true;
+    //    }
+
+   // }   
+
+
+//msg.oninput = function () {
+  //  connection.invoke("Typing");
+//}
+
+
